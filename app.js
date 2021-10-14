08d3ed07ee1a7f2baa1e115cbe543d918dd654a0
fahmiaga <70248462+fahmiaga@users.noreply.github.com>
2021-10-14 16:18:53 +0700

    add cors

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require("morgan");
 const methodOverride = require("method-override");
 const session = require("express-session");
 const flash = require("connect-flash");
+const cors = require("cors");
 
 // mongoose
 const mongoose = require("mongoose");
@@ -42,6 +43,7 @@ app.use(
 app.use(flash());
 app.use(logger("dev"));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
